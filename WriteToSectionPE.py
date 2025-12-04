4ccd856d645b3a9d74011f0ce586884639d5f93c
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-12-04 08:45:29 -0500

    Create WriteToSectionPE.py

---
+++ WriteToSectionPE.py
@@ -0,0 +1,4 @@
+"""
+python script that writes bytes (this will usually be the target dll) to a specific section of a PE file (this PE file should be the ReflectiveLoader)
+TODO: if it's easier to write this script in a different lanaguage than python, I should do so
+"""
