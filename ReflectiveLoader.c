e2a1fa73de4fbb5d41ff214c64e4f07cebadb90b
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-12-04 09:16:30 -0500

    Update ReflectiveLoader.c

--- ReflectiveLoader.c
+++ ReflectiveLoader.c
@@ -1,1 +1,11 @@
 /* ReflectiveLoader.c */
+/*
+1. Locate the PE headers
+2. allocate memory for the image
+3. map sections into the new memory
+4. apply relocations
+5. resolve imports
+6. fix protections
+7. run TLS callbacks
+8. call the module's entry point (DllMain)
+*/
